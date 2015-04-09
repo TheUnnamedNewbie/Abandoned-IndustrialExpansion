ef1e5541ff0fc07444efc043387e729872fd2767
TheUnnamedNewbie <Ann@iMac-van-Ann.local>
2015-04-09 11:08:48 +0200

    added fluids
    moved concrete to fluids

---
+++ prototypes/fluid/fluids.lua
@@ -0,0 +1,94 @@
+data:extend(
+{
+	 {
+    type = "fluid",
+    name = "graphite-sludge",
+    default_temperature = 25,
+    heat_capacity = "2KJ",
+    base_color = {r=0, g=0, b=0},
+    flow_color = {r=0.5, g=0.5, b=0.5},
+    max_temperature = 100,
+    icon = "",
+    pressure_to_speed_ratio = 0.4,
+    flow_to_energy_ratio = 0.59,
+    order = "a[fluid]-b[graphite-sludge]"
+  },
+    {
+    type = "fluid",
+    name = "chloride-gas",
+    default_temperature = 25,
+    heat_capacity = "0.6KJ",
+    base_color = {r=0, g=0, b=0},
+    flow_color = {r=0.5, g=0.5, b=0.5},
+    max_temperature = 100,
+    icon = "__base__/graphics/icons/fluid/crude-oil.png",
+    pressure_to_speed_ratio = 0.4,
+    flow_to_energy_ratio = 0.1,
+    order = "a[fluid]-b[chloride-gas]"
+  },
+    {
+    type = "fluid",
+    name = "sodium-hydroxide",
+    default_temperature = 25,
+    heat_capacity = "1KJ",
+    base_color = {r=0, g=0, b=0},
+    flow_color = {r=0.5, g=0.5, b=0.5},
+    max_temperature = 100,
+    icon = "__base__/graphics/icons/fluid/crude-oil.png",
+    pressure_to_speed_ratio = 0.4,
+    flow_to_energy_ratio = 0.59,
+    order = "a[fluid]-b[sodium-hydroxide]"
+  },  {
+    type = "fluid",
+    name = "hydrogen-gas",
+    default_temperature = 25,
+    heat_capacity = "1KJ",
+    base_color = {r=0, g=0, b=0},
+    flow_color = {r=0.5, g=0.5, b=0.5},
+    max_temperature = 100,
+    icon = "__base__/graphics/icons/fluid/crude-oil.png",
+    pressure_to_speed_ratio = 0.4,
+    flow_to_energy_ratio = 0.59,
+    order = "a[fluid]-b[hydrogen-gas]"
+  }, 
+    {
+    type = "fluid",
+    name = "sodium-alumate",
+    default_temperature = 25,
+    heat_capacity = "1KJ",
+    base_color = {r=0, g=0, b=0},
+    flow_color = {r=0.5, g=0.5, b=0.5},
+    max_temperature = 100,
+    icon = "__base__/graphics/icons/fluid/crude-oil.png",
+    pressure_to_speed_ratio = 0.4,
+    flow_to_energy_ratio = 0.59,
+    order = "a[fluid]-b[sodium-alumate]"
+  }, 
+    {
+    type = "fluid",
+    name = "concrete",
+    default_temperature = 25,
+    heat_capacity = "1KJ",
+    base_color = {r=0, g=0, b=0},
+    flow_color = {r=0.5, g=0.5, b=0.5},
+    max_temperature = 100,
+    icon = "__base__/graphics/icons/fluid/crude-oil.png",
+    pressure_to_speed_ratio = 0.4,
+    flow_to_energy_ratio = 0.59,
+    order = "a[fluid]-b[concrete]"
+  },
+    {
+    type = "fluid",
+    name = "hydrochloric-acid",
+    default_temperature = 25,
+    heat_capacity = "1KJ",
+    base_color = {r=0, g=0, b=0},
+    flow_color = {r=0.5, g=0.5, b=0.5},
+    max_temperature = 100,
+    icon = "__base__/graphics/icons/fluid/crude-oil.png",
+    pressure_to_speed_ratio = 0.4,
+    flow_to_energy_ratio = 0.59,
+    order = "a[fluid]-b[hydrochloric-acid]"
+  }
+}
+)

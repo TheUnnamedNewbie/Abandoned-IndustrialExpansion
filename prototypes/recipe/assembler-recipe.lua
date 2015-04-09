c01958a7a347a6e453c48a4e6899d0b9e6d04237
TheUnnamedNewbie <Ann@iMac-van-Ann.local>
2015-04-09 12:15:27 +0200

    added assembler-recipe

---
+++ prototypes/recipe/assembler-recipe.lua
@@ -0,0 +1,16 @@
+data:extend(
+{
+	{
+    type = "recipe",
+    category = "crafting",
+    name = "rebar",
+    enabled = "true",
+    energy_required = 10,
+    ingredients =
+    {
+      {"steel-plate", 1}
+    },
+    result = "rebar"
+  }
+}
+)
